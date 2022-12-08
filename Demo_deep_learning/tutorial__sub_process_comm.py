1d9b16f809567ba61aa35a31b899bbc62c0da3d7
YonV1943 曾伊言 <37322666+Yonv1943@users.noreply.github.com>
2022-12-08 18:19:38 +0800

    Add files via upload

---
+++ Demo_deep_learning/tutorial__sub_process_comm.py
@@ -0,0 +1,99 @@
+import multiprocessing
+import time
+from multiprocessing import Process, Pipe
+
+
+class SubEnv(Process):
+    def __init__(self, pipe):
+        super().__init__()
+        self.pipe = pipe
+
+        state = 0  # state = env.reset()
+        self.pipe.send(state)
+
+    def run(self):
+        for t in range(8):
+            action = self.pipe.recv()
+            state = action + 1
+            time.sleep(1)
+            self.pipe.send(state)
+
+
+class VecEnv(Process):
+    def __init__(self, pipes):
+        super().__init__()
+        self.pipes = pipes
+
+    def run(self):
+        states = [p.recv() for p in self.pipes]
+        actions = states
+
+        for i in range(8):
+            [p.send(a) for p, a in zip(self.pipes, actions)]
+            states = [p.recv() for p in self.pipes]
+            actions = states
+            print(';;;', states)
+
+
+def run():
+    print('Run Process')
+    num_workers = 4
+
+    pipes = [Pipe() for _ in range(num_workers)]
+    pipe0s, pipe1s = list(map(list, zip(*pipes)))
+
+    workers = [SubEnv(pipe0) for pipe0 in pipe0s]
+    learner = VecEnv(pipe1s)
+
+    process = [learner, ]
+    process.extend(workers)
+
+    [p.start() for p in process]
+    [p.join() for p in process]
+    [p.close() for p in process]
+
+
+def main():
+    print('Use Process to run Process')
+    process = Process(target=run, args=())
+    process.start()
+    process.join()
+    process.close()
+
+# if __name__ == '__main__':
+#     multiprocessing.set_start_method('spawn')
+#     run()  # run Process
+#     main()  # use Process to run Process
+
+
+
+# SuperFastPython.com
+# example of getting a result from an asynchronous task issued with apply_async
+from time import sleep
+from random import random
+from multiprocessing import Pool
+
+
+# task executed in a child process
+def task(value):
+    # generate a random value between 0 and 1
+    # block for a moment
+    sleep(value)
+    # return the generated value
+    return value
+
+
+# protect the entry point
+if __name__ == '__main__':
+    # create the process pool
+    with Pool(processes=4) as pool:
+        # issue a task asynchronously
+        result = pool.map_async(task, [1.5, 1.2, 1.3, 1.4])
+        result = result.get()
+        print(f'Got: {result}')
+        result = pool.map_async(task, [1.5, 1.2, 1.3, 1.4])
+        result = result.get()
+        print(f'Got: {result}')
+
+
+
--- Demo_deep_learning/tutorial__subp_vec_env_gym.py
+++ Demo_deep_learning/tutorial__subp_vec_env_gym.py
@@ -1,8 +1,10 @@
 import gym
 import torch
+import numpy as np
 import multiprocessing
+from typing import List
 from torch import Tensor
-from multiprocessing import Process, Pipe
+from multiprocessing import Process, Pipe, Queue
 
 
 def kwargs_filter(function, kwargs: dict) -> dict:
@@ -13,36 +15,75 @@ def kwargs_filter(function, kwargs: dict) -> dict:
     return {key: kwargs[key] for key in common_args}  # filtered kwargs
 
 
-class SubEnv(Process):
-    def __init__(self, pipe: Pipe, env_class, env_args: dict, device: torch.device):
+def build_vec_env(env_class, env_args: dict, gpu_id: int = -1):
+    env_args['gpu_id'] = gpu_id  # set gpu_id for vectorized env before build it
+    env_args.setdefault('num_envs', 1)
+    env_args.setdefault('max_step', 12345)
+
+    if_gym_env = env_class.__module__ == 'gym.envs.registration'  # is standard OpenAI Gym env
+    if_vec_env = env_args['num_envs'] > 1  # is vectorized env
+
+    if if_gym_env:  # is standard OpenAI Gym env
+        import gym
+        assert '0.18.0' <= gym.__version__ <= '0.25.2'  # pip3 install gym==0.24.0
+        gym.logger.set_level(40)  # Block warning
+        env_args['id'] = env_args['env_name']  # OpenAI gym build env by `gym.make(id=env_name)`
+        # env = env_class(id=env_args['env_name'])
+
+    if if_vec_env:
+        env = VecEnvAsync(env_class=env_class, env_args=env_args)
+        # env = VecEnvSync(env_class=env_class, env_args=env_args)
+        '''The following code inside VecEnv.__init__() -> SubEnv.run()
+        if self.env_class.__module__ == 'gym.envs.registration':  # is standard OpenAI Gym env
+            env = self.env_class(id=self.env_args['env_name'])
+        else:
+            env = self.env_class(**kwargs_filter(self.env_class.__init__, self.env_args.copy()))
+
+        '''
+    elif if_gym_env:
+        env = env_class(id=env_args['env_name'])
+    else:  # if not if_gym_env
+        env = env_class(**kwargs_filter(env_class.__init__, env_args.copy()))
+
+    for attr_str in ('env_name', 'num_envs', 'max_step', 'state_dim', 'action_dim', 'if_discrete'):
+        setattr(env, attr_str, env_args[attr_str])
+    return env
+
+
+class SubEnvSync(Process):
+    def __init__(self, pipe: Pipe, env_class, env_args: dict, env_id: int = 0):
         super().__init__()
         self.pipe = pipe
         self.env_class = env_class
         self.env_args = env_args
-        self.device = device
+        self.env_id = env_id
 
     def run(self):
         torch.set_grad_enabled(False)
 
-        # from elegantrl.train.config import build_vec_env
-        # env = build_vec_env(env_class=self.env_class, env_args=self.env_args)
-        from elegantrl.train.config import kwargs_filter
-        env = self.env_class(**kwargs_filter(self.env_class.__init__, self.env_args.copy()))
+        '''build env'''
+        if self.env_class.__module__ == 'gym.envs.registration':  # is standard OpenAI Gym env
+            env = self.env_class(id=self.env_args['env_name'])
+        else:
+            env = self.env_class(**kwargs_filter(self.env_class.__init__, self.env_args.copy()))
+
+        '''set env random seed'''
+        random_seed = self.env_id
+        np.random.seed(random_seed)
+        torch.manual_seed(random_seed)
 
         state = env.reset()
-        state = torch.as_tensor(state, device=self.device)
+        state = torch.tensor(state)
         self.pipe.send(state)
 
         while True:
             action = self.pipe.recv()
             state, reward, done, info_dict = env.step(action)
-            if done:
-                state = env.reset()
-            state = torch.as_tensor(state, device=self.device)
+            state = env.reset() if done else state
             self.pipe.send((state, reward, done, info_dict))
 
 
-class VecEnv:
+class VecEnvSync:
     def __init__(self, env_class: object, env_args: dict, gpu_id: int = -1):
         self.device = torch.device(f"cuda:{gpu_id}" if (torch.cuda.is_available() and (gpu_id >= 0)) else "cpu")
 
@@ -55,24 +96,23 @@ class VecEnv:
         self.if_discrete = env_args['if_discrete']  # discrete action or continuous action
 
         '''speed up with multiprocessing: Process, Pipe'''
-        pipes = [Pipe() for _ in range(self.num_envs)]
-        self.pipes, pipes = list(map(list, zip(*pipes)))
-        self.sub_envs = [SubEnv(pipe=pipe, env_class=env_class, env_args=env_args, device=self.device)
-                         for pipe in pipes]
+        self.pipes, pipes = list(zip(*[Pipe() for _ in range(self.num_envs)]))
+        self.sub_envs = [SubEnvSync(pipe=pipe, env_class=env_class, env_args=env_args, env_id=env_id)
+                         for env_id, pipe in enumerate(pipes)]
         [p.start() for p in self.sub_envs]
 
     def reset(self) -> Tensor:  # reset the agent in env
+        torch.set_grad_enabled(False)
+
         states = [p.recv() for p in self.pipes]
         states = torch.stack(states).to(self.device)
         return states
 
-    def step(self, action: Tensor) -> (Tensor, Tensor, Tensor, (dict,)):  # agent interacts in env
-        [p.send(a) for p, a in zip(self.pipes, action)]
-
-        state_reward_done_info_dict = [p.recv() for p in self.pipes]
-        states, rewards, dones, info_dicts = list(map(list, zip(*state_reward_done_info_dict)))
-
-        states = torch.stack(states).to(self.device)
+    def step(self, action: Tensor) -> (Tensor, Tensor, Tensor, List[dict]):  # agent interacts in env
+        for pipe, a in zip(self.pipes, action):
+            pipe.send(a)
+        states, rewards, dones, info_dicts = list(zip(*[p.recv() for p in self.pipes]))
+        states = torch.tensor(np.stack(states), dtype=torch.float32, device=self.device)
         rewards = torch.tensor(rewards, dtype=torch.float32, device=self.device)
         dones = torch.tensor(dones, dtype=torch.bool, device=self.device)
         return states, rewards, dones, info_dicts
@@ -81,36 +121,114 @@ class VecEnv:
         [p.terminate() for p in self.sub_envs]
 
 
-def demo__vec_env_with_gym_env():
-    print('Use Process to run Process')
-    num_envs = 4
-    env_name = 'BipedalWalker-v3'
+class SubEnvAsync(Process):
+    def __init__(self, pipe: Pipe, main_p: Pipe, env_class, env_args: dict, env_id: int = 0):
+        super().__init__()
+        self.pipe = pipe
+        self.main_p = main_p
+        self.env_class = env_class
+        self.env_args = env_args
+        self.env_id = env_id
 
+    def run(self):
+        torch.set_grad_enabled(False)
+
+        '''build env'''
+        if self.env_class.__module__ == 'gym.envs.registration':  # is standard OpenAI Gym env
+            env = self.env_class(id=self.env_args['env_name'])
+        else:
+            env = self.env_class(**kwargs_filter(self.env_class.__init__, self.env_args.copy()))
+
+        '''set env random seed'''
+        random_seed = self.env_id
+        np.random.seed(random_seed)
+        torch.manual_seed(random_seed)
+
+        state = env.reset()
+        state = torch.tensor(state)
+        self.main_p.send((self.env_id, state))
+
+        while True:
+            action = self.pipe.recv()
+            state, reward, done, info_dict = env.step(action)
+            state = env.reset() if done else state
+            self.main_p.send((self.env_id, state, reward, done, info_dict))
+
+
+class VecEnvAsync:
+    def __init__(self, env_class: object, env_args: dict, gpu_id: int = -1):
+        self.device = torch.device(f"cuda:{gpu_id}" if (torch.cuda.is_available() and (gpu_id >= 0)) else "cpu")
+
+        '''the necessary env information when you design a custom env'''
+        self.env_name = env_args['env_name']  # the name of this env.
+        self.num_envs = env_args['num_envs']  # the number of sub env in vectorized env.
+        self.max_step = env_args['max_step']  # the max step number in an episode for evaluation
+        self.state_dim = env_args['state_dim']  # feature number of state
+        self.action_dim = env_args['action_dim']  # feature number of action
+        self.if_discrete = env_args['if_discrete']  # discrete action or continuous action
+
+        '''speed up with multiprocessing: Process, Pipe'''
+        pipes, self.pipes = list(zip(*[Pipe(duplex=False) for _ in range(self.num_envs)]))
+        self.main_p, main_p = Pipe(duplex=False)
+        self.sub_envs = [SubEnvAsync(pipe=pipe, env_class=env_class, env_args=env_args, env_id=env_id, main_p=main_p)
+                         for env_id, pipe in enumerate(pipes)]
+        [p.start() for p in self.sub_envs]
+
+    def reset(self) -> Tensor:  # reset the agent in env
+        torch.set_grad_enabled(False)
+
+        _, states = self.get_orderly_zip_list_return()
+        states = torch.stack(states).to(self.device)
+        return states
+
+    def step(self, action: Tensor) -> (Tensor, Tensor, Tensor, List[dict]):  # agent interacts in env
+        for pipe, a in zip(self.pipes, action):
+            pipe.send(a)
+
+        _, states, rewards, dones, info_dicts = self.get_orderly_zip_list_return()
+        states = torch.tensor(np.stack(states), dtype=torch.float32, device=self.device)
+        rewards = torch.tensor(rewards, dtype=torch.float32, device=self.device)
+        dones = torch.tensor(dones, dtype=torch.bool, device=self.device)
+        return states, rewards, dones, info_dicts
+
+    def close(self):
+        [p.terminate() for p in self.sub_envs]
+
+    def get_orderly_zip_list_return(self):
+        res = [self.main_p.recv() for _ in range(self.num_envs)]
+        env_ids = [r[0] for r in res]
+        res = [res[i] for i in env_ids]  # orderly
+        return list(zip(*res))
+
+
+def demo__vec_env_with_gym_env(num_envs=8):
+    env_name = 'BipedalWalker-v3'
     env_class = gym.make
     env_args = {'env_name': env_name,
                 'num_envs': num_envs,
                 'max_step': 1600,
                 'state_dim': 24,
                 'action_dim': 4,
-                'if_discrete': False}
+                'if_discrete': False, }
 
-    env = VecEnv(env_class=env_class, env_args=env_args)
+    env = build_vec_env(env_class=env_class, env_args=env_args)
     device = env.device
     action_dim = env_args['action_dim']
 
     states = env.reset()
     assert isinstance(states, Tensor)
-    for t in range(8):
+
+    from time import time
+    timer = time()
+    for t in range(2 ** 12):
         actions = torch.rand((num_envs, action_dim), dtype=torch.float32, device=device)
         states, rewards, dones, _ = env.step(actions)
-        print(';;', rewards)
+        # print(';;', rewards)
     env.close()
+    print(f'NumEnvs: {num_envs:4} | UsedTime:  {time() - timer:9.4f}  {(time() - timer) / num_envs:9.4f}')
 
 
-def demo__vec_env_with_custom_env():
-    print('Use Process to run Process')
-    num_envs = 4
-
+def demo__vec_env_with_custom_env(num_envs=8):
     from elegantrl.envs.CustomGymEnv import PendulumEnv
     env_class = PendulumEnv
     env_args = {'env_name': 'CustomEnv-v0',
@@ -120,21 +238,54 @@ def demo__vec_env_with_custom_env():
                 'action_dim': 1,
                 'if_discrete': False}
 
-    env = VecEnv(env_class=env_class, env_args=env_args)
+    env = build_vec_env(env_class=env_class, env_args=env_args)
     device = env.device
     action_dim = env_args['action_dim']
 
     states = env.reset()
     assert isinstance(states, Tensor)
-    for t in range(8):
+
+    from time import time
+    timer = time()
+    for t in range(2 ** 12):
         actions = torch.rand((num_envs, action_dim), dtype=torch.float32, device=device)
         states, rewards, dones, _ = env.step(actions)
-        print(';;', rewards)
+        # print(';;', rewards)
     env.close()
+    print(f'NumEnvs: {num_envs:4} | UsedTime:  {time() - timer:9.4f}  {(time() - timer) / num_envs:9.4f}')
 
 
 if __name__ == '__main__':
     multiprocessing.set_start_method('spawn')
 
-    # demo__vec_env_with_gym_env()
-    demo__vec_env_with_custom_env()
+    ListNumEnvs = [2 ** n for n in range(9)]
+    for n in ListNumEnvs:
+        demo__vec_env_with_custom_env(num_envs=n)
+    for n in ListNumEnvs:
+        demo__vec_env_with_gym_env(num_envs=n)
+
+"""
+VecEnvSync
+NumEnvs:    2 | UsedTime:    13.9059     6.9529
+NumEnvs:    4 | UsedTime:    19.2333     4.8083
+NumEnvs:    8 | UsedTime:    32.5779     4.0722
+NumEnvs:   16 | UsedTime:    57.2705     3.5794
+NumEnvs:   32 | UsedTime:   105.3062     3.2908
+NumEnvs:   64 | UsedTime:   211.1338     3.2990
+
+VecEnvAsync1 `tensor[env_ids]`
+NumEnvs:    2 | UsedTime:    14.6936     7.3468
+NumEnvs:    4 | UsedTime:    20.9482     5.2370
+NumEnvs:    8 | UsedTime:    31.2089     3.9011
+NumEnvs:   16 | UsedTime:    51.7776     3.2361
+NumEnvs:   32 | UsedTime:    93.5009     2.9219
+NumEnvs:   64 | UsedTime:   178.4156     2.7877
+
+VecEnvAsync2 `get_orderly_zip_list_return`
+NumEnvs:    2 | UsedTime:    13.7087     6.8544
+NumEnvs:    4 | UsedTime:    20.6020     5.1505
+NumEnvs:    8 | UsedTime:    29.5131     3.6891
+NumEnvs:   16 | UsedTime:    53.8905     3.3682
+NumEnvs:   32 | UsedTime:    88.0039     2.7501
+NumEnvs:   64 | UsedTime:   162.9671     2.5464
+"""
